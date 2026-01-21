6b6cd01759b316da6d1cad89a8999f8d199d42c9
tiflu <j7321p@gmail.com>
2026-01-21 06:18:56 -0600

    Adds an incremental timer, and a stop button

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let timerDecrementTime = 0;
+let timer;
+
 window.addEventListener("load", () => {
     const dropdown = document.querySelector("#dropdown");
     dropdown.addEventListener("change",() => {
@@ -10,25 +13,60 @@ window.addEventListener("load", () => {
     })
 })
 
-function timer(ms) {
-    const textElement = document.querySelector("#timerText");
+function setDecrementTime(ms) {
+    timerDecrementTime = ms;
+}
+
+function startTimer() {
     const startTime = Date.now();
-    const endTime = new Date(startTime + ms).valueOf();
 
-    let timer = setInterval(() => {
-        ms = endTime - Date.now();
-        const currentHours = Math.floor(ms/(3600000));
-        let excessMs = ms % 3600000;
-        const currentMinutes = Math.floor(excessMs/60000);
-        excessMs %= 60000
-        const currentSeconds = Math.floor(excessMs/1000);
-        const currentMs = excessMs % 1000;
+    if (document.querySelector("#dropdown").value === "incremental") {
+        timer = setInterval(() => {
+            setTimerText(Date.now() - startTime);
+        }, 1000/60)
 
-        if (ms <= 0) {
-            clearInterval(timer);
-            textElement.innerText = "00:00:00.000";
-            return;
-        }
-        textElement.innerText = `${currentHours.toString().padStart(2, "0")}:${currentMinutes.toString().padStart(2, "0")}:${currentSeconds.toString().padStart(2, "0")}.${currentMs.toString().padStart(3, "0")}`;
-    }, 1000/60)
+    } else {
+        let ms = timerDecrementTime;
+        const endTime = new Date(startTime + ms).valueOf();
+
+        timer = setInterval(() => {
+            ms = endTime - Date.now();
+
+            if (ms <= 0) {
+                stopTimer();
+                return;
+            }
+
+            setTimerText(ms);
+
+        }, 1000 / 60)
+    }
+}
+
+function stopTimer() {
+    clearInterval(timer);
+    document.querySelector("#timerText").innerText = "00:00:00.000";
+}
+
+function getTime(ms) {
+    const map = new Map();
+
+    map.set("hours", Math.floor(ms / 3600000));
+    let excessMs = ms % 3600000;
+    map.set("minutes", Math.floor(excessMs / 60000));
+    excessMs %= 60000
+    map.set("seconds", Math.floor(excessMs / 1000));
+    map.set("ms", excessMs % 1000);
+    return map;
+}
+
+function setTimerText(ms) {
+    const textElement = document.querySelector("#timerText");
+    const time = getTime(ms);
+
+    textElement.innerText =
+        `${time.get("hours").toString().padStart(2, "0")}:` +
+        `${time.get("minutes").toString().padStart(2, "0")}:` +
+        `${time.get("seconds").toString().padStart(2, "0")}.` +
+        `${time.get("ms").toString().padStart(3, "0")}`;
 }
